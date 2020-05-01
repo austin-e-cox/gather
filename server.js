b01f9adf223cbd61db2cff1503b9f024ebead768
David <david@thejurne.com>
2020-05-01 12:16:53 -0700

    rm comments

--- server.js
+++ server.js
@@ -4,15 +4,11 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const mongoose = require('mongoose');
 
-
-// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://admin:75llama-hunter@ds031597.mlab.com:31597/heroku_mn5v1j8t');
 
-// Send every request to the React app
-// Define any API routes before this runs
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
