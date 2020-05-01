2e69b772a8ff23cc7570555380cce9ce0d5b99d8
David <david@thejurne.com>
2020-05-01 12:07:35 -0700

    add new creds to mLab

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose');
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://superuser:password1@ds031597.mlab.com:31597/heroku_mn5v1j8t');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://admin:75llama-hunter@ds031597.mlab.com:31597/heroku_mn5v1j8t');
 
 // Send every request to the React app
 // Define any API routes before this runs
