080a8d21cc42e2049b572b1241107e8f57bbd519
David <david@thejurne.com>
2020-05-04 16:18:25 -0700

    finish server setup

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI = "mongodb://admin:75llama-hunter@ds031597.mlab.com:31597/heroku_mn5v1j8t"
+};
---
+++ server.js
@@ -0,0 +1,25 @@
+// Pull in required deps...
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+// Initialize app using express...
+const app = express();
+
+// Apply bodyParser so it can be used...
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+
+//Pull in our MongoDB URI and connect to DB...
+const db = require('./config/keys').mongoURI;
+
+mongoose.connect(db, { useNewUrlParser: true })
+  .then(console.log('Succesfully Connected to DB...'))
+  .catch(err => console.log(err));
+
+// Set and listen to PORT...
+const port = PROCESS.ENV.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}!`));
