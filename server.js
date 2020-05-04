71eb17fe59746debae2d8c502886a331a8f1c997
David <david@thejurne.com>
2020-05-04 15:39:14 -0700

    add more login auth

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
+// Require Packages
 const express = require('express');
+const session = require('express-session');
 const path = require('path');
-const PORT = process.env.PORT || 3001;
+
+// Require Passport
+const passport = require('./config/passport');
+
+// Set Up Ports and Require Models
+const PORT = process.env.PORT || 8080;
+const db = require('./models');
+
+// Create Express App and Configure Auth Middleware
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// Sessions Track User Login State
+app.use(session({secret: 'keyboard cat', resave: true, saveUninitialized: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Require Routes
+require('./routes/api-routes.js')(app);
+
+
 const mongoose = require('mongoose');
 
 if (process.env.NODE_ENV === 'production') {
@@ -11,6 +34,10 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://admin:75llama-hunter@ds03
 
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
+
+app.post('/', (req, res) => {
+  res.send(req.data);
 });
 
 app.listen(PORT, function() {
