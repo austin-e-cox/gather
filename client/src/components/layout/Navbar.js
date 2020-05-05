192f2772b86c009fa9c5db320a609200088553c1
David <david@thejurne.com>
2020-05-04 18:04:57 -0700

    add default navbar and landing

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => (
+  <>
+  <h1>Navbar</h1>
+  <Link to="/">Landing</Link>
+  </>
+);
+
+export default Navbar;
