7f00f1616b09d4916863ebc9a965525eee2550e3
David <david@thejurne.com>
2020-05-04 17:27:49 -0700

    add passport to server

--- server.js
+++ server.js
@@ -2,6 +2,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+
+// Pull in users API...
+const users = require('./routes/api/users');
 
 // Initialize app using express...
 const app = express();
@@ -18,6 +22,15 @@ const db = require('./config/keys').mongoURI;
 mongoose.connect(db, { useNewUrlParser: true })
   .then(() => console.log('Succesfully Connected to DB...'))
   .catch(err => console.log(err));
+
+// Passport Middleware...
+app.use(passport.initialize());
+
+// Passport Config...
+require('./config/passport')(passport);
+
+// Routes
+app.use('/api/users', users);
 
 // Set and listen to PORT...
 const port = process.env.PORT || 5000;
