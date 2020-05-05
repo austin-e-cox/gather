3ad636862cb7af014d59bd3477eea6ceec4f8ce5
David <david@thejurne.com>
2020-05-04 17:05:08 -0700

    add user register route

--- routes/api/users.js
+++ routes/api/users.js
@@ -11,3 +11,40 @@ const validateLoginInput = require('../../validation/login');
 
 // Load User model...
 const User = require('../../models/User');
+
+// Create register endpoint...
+router.post('/register', (req, res) => {
+
+  // Form validation...
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check validation...
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  // Find User if already exists...
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Email already exists..." });
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      });
+
+      // Hash password before sending to DB...
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
