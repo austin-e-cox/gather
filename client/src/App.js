4880ab6a7a68354926f2194e34d9985ec87ec3c1
peterboldyrev <53663056+peterboldyrev@users.noreply.github.com>
2020-05-05 21:04:23 -0700

    upd to appjs to merge with master without errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,5 @@
-<<<<<<< HEAD
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Navbar from "./components/Navbar";
-
-class App extends Component {
-  render() {
-    return (
-      <>
-      <Navbar/>
-=======
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './store';
-import jwt_decode from "jwt-decode";
-import setAuthToken from "./utils/setAuthToken";
-import { setCurrentUser, logoutUser } from "./actions/authActions";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import './App.css';
 
@@ -24,62 +7,18 @@ import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
-import PrivateRoute from "./components/private-route/PrivateRoute";
-import Dashboard from "./components/dashboard/Dashboard";
-
-// Check for token to keep user logged in
-if (localStorage.jwtToken) {
-  // Set auth token header auth
-  const token = localStorage.jwtToken;
-  setAuthToken(token);
-  // Decode token and get user info and exp
-  const decoded = jwt_decode(token);
-  // Set user and isAuthenticated
-  store.dispatch(setCurrentUser(decoded));
-  // Check for expired token
-  const currentTime = Date.now() / 1000; // to get in milliseconds
-  if (decoded.exp < currentTime) {
-    // Logout user
-    store.dispatch(logoutUser());
-    // Redirect to login
-    window.location.href = "./login";
-  }
-}
 
 function App() {
   return (
-<<<<<<< HEAD
     <Router>
->>>>>>> 1acf53c9bfa05c797149c41de7751bbdf99665aa
       <div className="App">
         <Navbar />
         <Route exact path="/" component={Landing} />
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
       </div>
-<<<<<<< HEAD
-      </>
-    );
-  }
-=======
     </Router>
-=======
-    <Provider store={store}>
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-          <Switch>
-            <PrivateRoute exact path="/dashboard" component={Dashboard} />
-          </Switch>
-        </div>
-      </Router>
-    </Provider>
->>>>>>> 4e12c7c8f57b74517e7139e0677aad6b7230057b
   );
->>>>>>> 1acf53c9bfa05c797149c41de7751bbdf99665aa
 }
 
 export default App;
