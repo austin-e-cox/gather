887e756f41dc06925bebb867a25f3aca73065803
Austin Cox <austin.ed.cox@gmail.com>
2020-05-06 22:20:12 -0700

    merge

--- models/User.js
+++ models/User.js
@@ -1,9 +1,5 @@
 // Pull in required deps...
-<<<<<<< Updated upstream
 const { v4: uuidv4 } = require('uuid');
-=======
-import { v4  as uuidv4 } from uuid;
->>>>>>> Stashed changes
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -25,11 +21,7 @@ const UserSchema = new Schema({
     type: Date,
     default: Date.now
   },
-<<<<<<< Updated upstream
   userId:{
-=======
-  userId: {
->>>>>>> Stashed changes
     type: String,
     default: uuidv4()
   }
