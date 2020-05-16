5820488a6c2eaf5beb8fc51d1e45e74bee05eb35
Ally Burk <allylynn@uw.edu>
2020-05-16 11:11:33 -0700

    adding part of the About page

---
+++ client/src/components/layout/About.js
@@ -0,0 +1,42 @@
+
+
+
+
+
+<div>
+  <section classname="pb_cover overflow-hidden cover-bg-indigo cover-bg-opacity text-left pb_gradient_v1 pb_slant-light" id="section-home">
+    <div classname="container">
+      <div classname="row align-items-center justify-content-center">
+        <div classname="col-12">
+          <div classname="container bootstrap-snippet header-container">
+            <div classname="bg-white">
+              <div classname="container py-5">
+                <div classname="media col-md-10 col-lg-8 col-xl-7 p-0 my-4 mx-auto">
+                  <div classname="media-body ml-5">
+                    <h4 classname="font-weight-bold mb-4">About Us</h4>
+                    <div classname="text-muted mb-4">
+                      Welcome to Gather! A site that connects us to one another through technology and creativity.
+                      The importance of social interaction is something that has helped shape who we are as people for as long as we have been around. 
+                      Year after year, the way we forge relationships with others changes with our lives. We get busy, it becomes difficult to go out and meet
+                      people and it can become challenging to find time to have meaningful and fun interactions with like minded people.
+                      Gather is here to help with that! Gather is a great platform that brings people who share common interests together, allowing 
+                      users to connect on a level that is not only fun but also encourages social growth. 
+                      Gather is here to help people connect on a meaningful human level and to remind 
+                      us all that at the end of the day, we are all human and we have more in common than we think!
+                      Connect with users around the country to come together and have in depth discussions about subjects that you care about. 
+                      Chat with users hundreds of miles away for some simple cooking tips and tricks! Have a conversation with someone in an entirely different
+                      place that you would never have thought to have common ground with.
+                      Whether you want to share ideas, hobbies, or just want to find people to have a good laugh with, Gather is here to help. 
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  );
+  {'}'}
+</div>
