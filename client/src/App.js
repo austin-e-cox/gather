7879e31e01af930b3c48f8aa4ed2d34f92d14776
Austin Cox <austin.ed.cox@gmail.com>
2020-05-19 20:40:11 -0700

    added unstyled chat components, and updated package to include socket.io and start the chat server

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import PrivateRoute from "./components/private-route/PrivateRoute";
 import Dashboard from "./components/dashboard/Dashboard";
+import Chat from "./components/Chat"
 
 // Check for token to keep user logged in
 if (localStorage.jwtToken) {
@@ -43,6 +44,7 @@ function App() {
           <Route exact path="/login" component={Login} />
           <Switch>
             <PrivateRoute exact path="/dashboard" component={Dashboard} />
+            <PrivateRoute exact path="/chat" component={Chat} />
           </Switch>
         </div>
       </Router>
