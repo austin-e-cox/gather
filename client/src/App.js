5d8aaac4695860f161b3c94443555ef3c36a4a69
Ally Burk <allylynn@uw.edu>
2020-05-20 19:42:33 -0700

    fixing routes and testing functions

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,59 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
 
 import './App.css';
-
 
 import Navbar from "./components/layout/Navbar";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import PrivateRoute from "./components/private-route/PrivateRoute";
+import Dashboard from "./components/dashboard/Dashboard";
+import Chat from "./components/Chat"
 import Interest from "./components/layout/Interest";
 import About from "./components/layout/About";
 
-class App extends React.Component {
-  render(){
-    return (
+// Check for token to keep user logged in
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+  // Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // Redirect to login
+    window.location.href = "./login";
+  }
+}
+
+function App() {
+  return (
+    <Provider store={store}>
       <Router>
         <div className="App">
           <Navbar />
           <Route exact path="/" component={Register} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/about" component={About} />
-          <Route exact path="/interests" component={Interest} />
+          <Switch>
+            <PrivateRoute exact path="/dashboard" component={Dashboard} />
+            <PrivateRoute exact path="/chat" component={Chat} />
+            <PrivateRoute exact path="/interest" component={Interest} />
+          </Switch>
         </div>
       </Router>
-    );
-  }
-  
+    </Provider>
+  );
 }
 
 export default App;
