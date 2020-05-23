e0c814cee6d176e060e334e2bf705f1c7311d8a8
David <david@thejurne.com>
2020-05-23 10:08:39 -0700

    add build?

---
+++ client/build/precache-manifest.123f109e0bbf76af2b9e2676a7ff9749.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab98b5f1e1fbf167c381f245a834c705",
+    "url": "/index.html"
+  },
+  {
+    "revision": "451fb562a23b7d6956ce",
+    "url": "/static/css/main.4687378e.chunk.css"
+  },
+  {
+    "revision": "52f2b5f494ab4f6b5700",
+    "url": "/static/js/2.b3d2114b.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.b3d2114b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "451fb562a23b7d6956ce",
+    "url": "/static/js/main.4714c12f.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
