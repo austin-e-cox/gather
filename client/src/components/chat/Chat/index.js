a3e1ea6e59cf1df4f8256bc07676a8be265f39a8
Austin Cox <austin.ed.cox@gmail.com>
2020-05-26 19:34:42 -0700

    .

--- client/src/components/chat/Chat/index.js
+++ client/src/components/chat/Chat/index.js
@@ -72,7 +72,9 @@ class Chat extends React.Component {
 
     socket.on('login', (data) => {
       // data should have active users, previous messages and we should set that to the state below
-      console.log("MSG LOG",data.messageLog)
+      console.log("MSG LOG", data.messageLog)
+      console.log("GROUPS")
+      console.log(user.groups);
       connected = true;
       // Display the welcome message
       let message = `Welcome to ${this.props.groupName}`;
